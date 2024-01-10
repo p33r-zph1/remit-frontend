6204bbec50e7a27144728326999472d7311e78d9
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2024-01-10 17:52:07 +0800

    fix: eslint & postcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,25 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime', // Runtime prefers to be at the last
     'prettier', // Prettier must be last
   ],
   ignorePatterns: [
     'dist',
     '.eslintrc.cjs',
     'prettier.config.cjs',
-    'postcss.config.cjs',
+    'postcss.config.js',
+    'vite.config.ts',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -22,7 +30,12 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint', 'react-refresh', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
