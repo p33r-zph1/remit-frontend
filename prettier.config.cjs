e6915f0200e34407659089eb250e8f12a687f70b
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2024-01-10 17:09:03 +0800

    feat: install prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Options} */
+const config = {
+  arrowParens: 'avoid',
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'auto',
+};
+
+module.exports = config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 export default function App() {
   return (
     <div className="container mx-auto px-4 max-w-xl md:px-8 xl:px-0">
-
       <div className="min-h-screen flex flex-col gap-16 justify-center items-center">
         <img src="/remit.svg" alt="Remit logo" />
 
         <div className="w-full flex flex-col gap-5 p-8 rounded-lg border border-[#EBEEF1] bg-white shadow-md">
-        
-          <h1 className="text-[#0E1E2F] text-3xl font-extrabold">
-            Log in
-          </h1>
+          <h1 className="text-[#0E1E2F] text-3xl font-extrabold">Log in</h1>
 
           <label className="form-control w-full">
             <div className="label">
@@ -20,7 +16,9 @@ export default function App() {
 
           <label className="form-control w-full">
             <div className="label">
-              <span className="label-text text=[#757F87] font-bold">Password</span>
+              <span className="label-text text=[#757F87] font-bold">
+                Password
+              </span>
             </div>
             <input type="password" className="input input-bordered w-full" />
           </label>
@@ -29,10 +27,8 @@ export default function App() {
             {false && <span className="loading loading-spinner"></span>}
             Login
           </button>
-
         </div>
       </div>
     </div>
-
-  )
+  );
 }
