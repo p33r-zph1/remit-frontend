bfbc66325c7e39c1c7bce8f69b164f4721cd67ed
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2024-01-30 21:08:28 +0800

    feat: maps compont now supports marker and circle radius

--- src/components/Location/MapsAPI.tsx
+++ src/components/Location/MapsAPI.tsx
@@ -1,7 +1,19 @@
-import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
-import type { GoogleMapProps, Libraries } from '@react-google-maps/api';
+import {
+  Circle,
+  GoogleMap,
+  Marker,
+  useLoadScript,
+} from '@react-google-maps/api';
+import type {
+  CircleProps,
+  GoogleMapProps,
+  Libraries,
+} from '@react-google-maps/api';
 import { useCallback, useMemo, useRef, useState } from 'react';
+import { coerce } from 'zod';
+
 import PlacesAutocomplete from '../Autocomplete/PlacesAutocomplete';
+import { cx } from '../../utils';
 
 type LatLng = GoogleMapProps['center'];
 type MapOptions = GoogleMapProps['options'];
@@ -10,10 +22,9 @@ const libraries: Libraries = ['places'];
 
 export default function TransferMap() {
   const mapRef = useRef<GoogleMap | undefined>();
-  const [range, setRange] = useState('100');
+  const [radius, setRadius] = useState(0);
 
   const [meetUpLocation, setMeetUpLocation] = useState<LatLng>();
-  console.log({ meetUpLocation });
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_MAPS_JS_API,
@@ -27,13 +38,31 @@ export default function TransferMap() {
     }),
     []
   );
+  const radiusOptions = useMemo(() => {
+    if (radius <= 250) return closeOptions;
+    if (radius <= 500) return middleOptions;
+
+    return farOptions;
+  }, [radius]);
+
+  const zoomLevel = useMemo(() => {
+    if (!meetUpLocation) return 10;
+    if (radius === 0) return 17;
+    if (radius <= 250) return 16.5;
+    if (radius <= 500) return 16;
+    if (radius <= 750) return 15.5;
+    if (radius <= 1000) return 15;
+
+    return 13;
+  }, [meetUpLocation, radius]);
 
   const options = useMemo<MapOptions>(
     () => ({
       disableDefaultUI: true,
       clickableIcons: false,
+      zoom: zoomLevel,
     }),
-    []
+    [zoomLevel]
   );
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -59,30 +88,77 @@ export default function TransferMap() {
         <input
           type="range"
           min={0}
-          max={400}
-          value={range}
-          onChange={e => setRange(e.target.value)}
-          className="range range-primary range-sm md:range-md"
-          step="100"
+          disabled={!meetUpLocation}
+          max={1000}
+          value={radius}
+          onChange={e => setRadius(coerce.number().parse(e.target.value))}
+          className={cx(
+            `range range-sm md:range-md`,
+            meetUpLocation && 'range-primary'
+          )}
+          step="250"
         />
         <div className="flex w-full justify-between px-2 text-xs">
           <span>0m</span>
-          <span>100m</span>
-          <span>200m</span>
-          <span>300m</span>
-          <span>400m</span>
+          <span>250m</span>
+          <span>500m</span>
+          <span>750m</span>
+          <span>1km</span>
         </div>
       </div>
 
       <GoogleMap
-        zoom={13}
+        zoom={10}
         center={center}
         mapContainerClassName="h-56 md:h-[50vh] w-full"
         options={options}
         onLoad={onload}
       >
-        {meetUpLocation && <Marker position={meetUpLocation} />}
+        {meetUpLocation && (
+          <>
+            <Marker position={meetUpLocation} />
+
+            {coerce.number().safeParse(radius).success && (
+              <Circle
+                center={meetUpLocation}
+                radius={radius}
+                options={radiusOptions}
+              />
+            )}
+          </>
+        )}
       </GoogleMap>
     </div>
   );
 }
+
+const defaultOptions: CircleProps['options'] = {
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+};
+const closeOptions: CircleProps['options'] = {
+  ...defaultOptions,
+  zIndex: 3,
+  fillOpacity: 0.05,
+  strokeColor: '#1165EF',
+  fillColor: '#1165EF',
+};
+
+const middleOptions: CircleProps['options'] = {
+  ...defaultOptions,
+  zIndex: 2,
+  fillOpacity: 0.05,
+  strokeColor: '#de8800',
+  fillColor: '#de8800',
+};
+const farOptions: CircleProps['options'] = {
+  ...defaultOptions,
+  zIndex: 1,
+  fillOpacity: 0.05,
+  strokeColor: '#cf3452',
+  fillColor: '#cf3452',
+};
