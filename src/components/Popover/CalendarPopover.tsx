eba61f79c76264642ce14bf9f04948fb38cbd4fc
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2024-01-31 10:51:31 +0800

    fix: calendar popver style

--- src/components/Popover/CalendarPopover.tsx
+++ src/components/Popover/CalendarPopover.tsx
@@ -15,60 +15,58 @@ export default function CalendarPopover() {
   const endDate = addHours(startDate, durationInHr);
 
   return (
-    <div className="my-2">
-      <Popover className="relative">
-        {({ open }) => (
-          <>
-            <Popover.Button
-              className={cx(
-                'flex w-full flex-row space-x-2 rounded-md border border-slate-200 px-1 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-primary md:text-base'
-              )}
-            >
-              <CalendarIcon
-                className={cx(open ? 'text-black' : 'text-black/70', 'h-5 w-5')}
-                aria-hidden="true"
-              />
+    <Popover className="my-2">
+      {({ open }) => (
+        <>
+          <Popover.Button
+            className={cx(
+              'flex w-full flex-row space-x-2 rounded-md border border-slate-200 px-1 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-primary md:text-base'
+            )}
+          >
+            <CalendarIcon
+              className={cx(open ? 'text-black' : 'text-black/70', 'h-5 w-5')}
+              aria-hidden="true"
+            />
 
-              <span className="font-semibold">
-                {format(startDate, 'MMMM dd, yyyy')}
-                {` `}
-                <span className="tracking-tighter">
-                  {format(startDate, 'h:mm a')}
-                  {' - '}
-                  {format(endDate, 'h:mm a')}{' '}
-                  <small className="text-gray-400">{`(${durationInHr}hr)`}</small>
-                </span>
+            <span className="font-semibold">
+              {format(startDate, 'MMMM dd, yyyy')}
+              {` `}
+              <span className="tracking-tighter">
+                {format(startDate, 'h:mm a')}
+                {' - '}
+                {format(endDate, 'h:mm a')}{' '}
+                <small className="text-gray-400">{`(${durationInHr}hr)`}</small>
               </span>
-            </Popover.Button>
+            </span>
+          </Popover.Button>
 
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform rounded-md bg-zinc-50 lg:max-w-md">
-                <div className="overflow-hidden rounded-lg p-4 shadow-lg ring-1 ring-black/5">
-                  <DateCalendar day={startDate} onChange={setStartDate} />
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform rounded-md bg-zinc-50 lg:max-w-md">
+              <div className="overflow-hidden rounded-lg p-4 shadow-lg ring-1 ring-black/5">
+                <DateCalendar day={startDate} onChange={setStartDate} />
 
-                  <div className="divider" />
+                <div className="divider" />
 
-                  <DateTime
-                    title="Time & Duration"
-                    today={startDate}
-                    onChange={setStartDate}
-                    durationInHr={durationInHr}
-                    setDurationInHr={setDurationInHr}
-                  />
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </>
-        )}
-      </Popover>
-    </div>
+                <DateTime
+                  title="Time & Duration"
+                  today={startDate}
+                  onChange={setStartDate}
+                  durationInHr={durationInHr}
+                  setDurationInHr={setDurationInHr}
+                />
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
   );
 }
