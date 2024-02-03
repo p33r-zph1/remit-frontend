4f56e6fdb3e750dfc3e968b0d2bff9f7b11d842e
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2024-02-03 19:29:27 +0800

    refactor: configs

--- src/config/auth.config.ts
+++
@@ -1,8 +0,0 @@
-import { ResourcesConfig } from 'aws-amplify';
-
-export const cognitoAuth: ResourcesConfig['Auth'] = {
-  Cognito: {
-    userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-    userPoolClientId: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID,
-  },
-};
--- src/main.tsx
+++ src/main.tsx
@@ -6,21 +6,31 @@ import { Amplify } from 'aws-amplify';
 import { defaultStorage } from 'aws-amplify/utils';
 import { cognitoUserPoolsTokenProvider } from 'aws-amplify/auth/cognito';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { routeTree } from './routeTree.gen';
 
 import useAuth from './hooks/useAuth';
 import { AuthProvider } from './contexts/AuthContext';
 
-import { cognitoAuth } from './config/auth.config';
-import { queryClient } from './config/query.config';
-
 Amplify.configure({
-  Auth: cognitoAuth,
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID,
+    },
+  },
 });
 
 cognitoUserPoolsTokenProvider.setKeyValueStorage(defaultStorage);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 const router = createRouter({
   routeTree,
